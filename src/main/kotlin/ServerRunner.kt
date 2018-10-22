e1060bbe8e875b8f7d56e4159a3a085b75d5e209
kyle.boos <kyle.boos@workday.com>
2018-10-21 19:12:24 -0700

    create fake all character json route

--- src/main/kotlin/ServerRunner.kt
+++ src/main/kotlin/ServerRunner.kt
@@ -5,7 +5,8 @@ class ServerRunner {
 
     fun run() {
         setEnvironmentPort()
-        getHome()
+        setupHomeRoute()
+        setupAllCharactersJsonRoute()
     }
 
     private fun setEnvironmentPort() {
@@ -14,9 +15,95 @@ class ServerRunner {
         }
     }
 
-    private fun getHome() {
+    private fun setupHomeRoute() {
         get("/") {
             "Welcome to the Tekken Lab"
+        }
+    }
+
+    private fun setupAllCharactersJsonRoute() {
+        get("/all") {
+            """
+                {
+	"characters": [{
+			"heihachi": [{
+					"basicMoves": [{
+							"command": "1",
+							"hitType": "high",
+							"damage": "7",
+							"startUpFrame": "10",
+							"onBlock": "+1",
+							"onHit": "+8",
+							"onCounterHit": "+8",
+							"notes": ""
+						},
+						{
+							"command": "d/f+1",
+							"hitType": "mid",
+							"damage": "13",
+							"startUpFrame": "13~14",
+							"onBlock": "-1~0",
+							"onHit": "+9~+10",
+							"onCounterHit": "+9~+10",
+							"notes": ""
+						}
+					]
+				},
+				{
+					"specialMoves": [{
+						"command": "in rage d+1+2",
+						"hitType": "mid",
+						"damage": "55",
+						"startUpFrame": "20 pc8~17",
+						"onBlock": "-22",
+						"onHit": "KND",
+						"onCounterHit": "KND",
+						"notes": "Rage art"
+					}]
+				}
+			]
+		},
+		{
+			"kazuya": [{
+					"basicMoves": [{
+							"command": "1",
+							"hitType": "high",
+							"damage": "7",
+							"startUpFrame": "10",
+							"onBlock": "+1",
+							"onHit": "+8",
+							"onCounterHit": "+8",
+							"notes": ""
+						},
+						{
+							"command": "d/f+1",
+							"hitType": "mid",
+							"damage": "13",
+							"startUpFrame": "13~14",
+							"onBlock": "-1~0",
+							"onHit": "+9~+10",
+							"onCounterHit": "+9~+10",
+							"notes": ""
+						}
+					]
+				},
+				{
+					"specialMoves": [{
+						"command": "in rage d+1+2",
+						"hitType": "mid",
+						"damage": "55",
+						"startUpFrame": "20 pc8~17",
+						"onBlock": "-22",
+						"onHit": "KND",
+						"onCounterHit": "KND",
+						"notes": "Rage art"
+					}]
+				}
+			]
+		}
+	]
+}
+            """.trimIndent()
         }
     }
 
